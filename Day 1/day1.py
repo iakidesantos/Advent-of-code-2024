e8b7368b4e8b509f42817003d7522114159cd206
iakidesantos <iakidesantos@gmail.com>
2025-07-24 15:51:34 -0600

    Add files via upload

---
+++ Day 1/day1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 24 12:24:18 2025
+
+@author: Asus
+"""
+
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/iakidesantos/Advent-of-code-2024/refs/heads/main/day1_input.txt'
+df = pd.read_csv(url, sep='\s+', header=None)
+df_sorted = df.apply(sorted)
+df_sorted['diff'] = abs(df_sorted[1]-df_sorted[0])
+result = sum(df_sorted['diff'])
+print(result)
